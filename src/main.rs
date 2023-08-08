050d1256dc63e0208213699b70c11154ae47ada9
rooot <hey@rooot.gay>
2023-08-08 21:05:07 +0200

    feat: initial commit
    Signed-off-by: rooot <hey@rooot.gay>

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use console::Term;
+use owo_colors::OwoColorize;
+use std::io::{Read, Write};
+use std::process::{exit, Command};
+
+fn main() {
+    let mut args: Vec<String> = std::env::args().collect();
+
+    let cmd = if args.len() > 1 {
+        args[1].to_string()
+    } else {
+        eprintln!(
+            "{} {}",
+            get_prefix().red(),
+            "no command specified!".bright_magenta()
+        );
+        exit(1);
+    };
+
+    args.drain(0..2);
+    let args = args;
+
+    let hostname = match Command::new("hostname").output() {
+        Ok(output) => match String::from_utf8(output.stdout) {
+            Ok(hostname) => hostname.trim().to_string(),
+            Err(_) => "localhost".to_string(),
+        },
+        Err(_) => "localhost".to_string(),
+    };
+
+    let mut command = Command::new(cmd.clone());
+    command.args(args.clone());
+
+    // is there a cleaner way to do this?
+    let command_string = format!("{} {}", cmd, args.join(" "));
+
+    println!(
+        "{} {} {}.",
+        get_prefix().red(),
+        "your current hostname is:".yellow(),
+        hostname.bright_green().bold()
+    );
+    print!(
+        " {} {} {} {}{} {} ",
+        "do you".yellow(),
+        "*REALLY*".red().italic().bold(),
+        "want to run".yellow(),
+        command_string.trim().red(),
+        "?".yellow(),
+        "(y/n)".yellow()
+    );
+
+    // flush stdout to display the prompt
+    let _ = std::io::stdout().flush();
+
+    if read_char() == 'y' {
+        command.spawn().expect("failed to run command");
+    } else {
+        println!(
+            "{} {}",
+            get_prefix().red(),
+            "oki, won't execute!".bright_magenta()
+        );
+    }
+}
+
+fn get_prefix() -> String {
+    format!("[{}]", env!("CARGO_CRATE_NAME"))
+}
+
+fn read_char() -> char {
+    match Term::stdout().read_char() {
+        Ok(c) => {
+            println!();
+            c
+        }
+        Err(_) => std::io::stdin()
+            .bytes()
+            .next()
+            .and_then(|result| result.ok())
+            .map(|byte| byte as char)
+            .unwrap(),
+    }
+}
